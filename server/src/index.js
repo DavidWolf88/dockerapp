97e6bd55be5c5dd1522c2ecf59e2aab609ef6248
dawid <dawid@dawid-vps.smartvps.pl>
2020-02-07 13:10:54 +0100

    final

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8000;
 const HOST = '0.0.0.0';
-const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/public');
+//const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
+const CLIENT_BUILD_PATH = path.join(__dirname);
 const app = express();
 
 app.use(express.static(CLIENT_BUILD_PATH));
@@ -18,6 +19,11 @@ app.get('/api', (req, res) => {
 
 app.get('*', function(request, response) {
   response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+//  response.sendFile(path.join('index.html'));
+// response.send('ok !');
+//response.send(process.env);
+//response.sendFile('/app/server/src/index.html');
+//response.sendFile('/app/client/build/index.html');
 });
 
 app.listen(PORT, HOST);
